c7fc2df836610f52fc9c3c4bb389339ea63de109
leocafo87 <leonardo.castellanos.forero@gmail.com>
2023-12-02 12:50:17 -0500

    Clasificación de Créditos Riesgosos usando SVM

--- preguntas.py
+++ preguntas.py
@@ -1,10 +1,8 @@
 """
 Clasificación de Créditos Riesgosos usando SVM
 -----------------------------------------------------------------------------------------
-
 Construya un modelo de clasificación que permita determinar si un crédito es riesgoso
 o no.
-
 Las entidades financieras desean mejorar sus procedimientos de aprobación de créditos con
 el fin de disminuir los riesgos de no pago de la deuda, lo que acarrea pérdidas a la 
 entidad. El problema real consiste en poder decidir si se aprueba o no un crédito 
@@ -152,20 +150,19 @@ def pregunta_01():
     En esta función se realiza la carga de datos.
     """
     # Lea el archivo `german.csv` y asignelo al DataFrame `df`
-    df = ____
+    df = pd.read_csv('german.csv')
 
     # Asigne la columna `default` a la variable `y`.
-    ____ = ____
+    y = df['default']
 
     # Asigne una copia del dataframe `df` a la variable `X`.
-    ____ = ____.____()
+    X = df.copy()
 
     # Remueva la columna `default` del DataFrame `X`.
-    ____.____(____)
+    X.drop('default', axis=1, inplace=True)
 
     # Retorne `X` y `y`
     return X, y
-
 
 def pregunta_02():
     """
@@ -173,22 +170,20 @@ def pregunta_02():
     """
 
     # Importe train_test_split
-    from ____ import ____
+    from sklearn.model_selection import train_test_split
 
     # Cargue los datos de ejemplo y asigne los resultados a `X` y `y`.
     X, y = pregunta_01()
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 123. Use 100 patrones para la muestra de prueba.
-    (X_train, X_test, y_train, y_test,) = ____(
-        ____,
-        ____,
-        test_size=____,
-        random_state=____,
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=100, random_state=123
     )
 
     # Retorne `X_train`, `X_test`, `y_train` y `y_test`
     return X_train, X_test, y_train, y_test
+
 
 
 def pregunta_03():
@@ -200,7 +195,10 @@ def pregunta_03():
     # Importe SVC
     # Importe OneHotEncoder
     # Importe Pipeline
-    from ____ import ____
+    from sklearn.compose import make_column_transformer
+    from sklearn.svm import SVC
+    from sklearn.preprocessing import OneHotEncoder
+    from sklearn.pipeline import Pipeline
 
     # Cargue las variables.
     X_train, _, y_train, _ = pregunta_02()
@@ -209,26 +207,24 @@ def pregunta_03():
     # tipo texto. Use make_column_selector para seleccionar las columnas. Las
     # columnas numéricas no deben ser transformadas.
     columnTransformer = make_column_transformer(
-        (
-            ____(),
-            ____(____=____),
-        ),
-        remainder=____,
+        (OneHotEncoder(), make_column_selector(dtype_include='object')),
+        remainder='passthrough'
     )
 
     # Cree un pipeline que contenga el columnTransformer y el modelo SVC.
-    pipeline = ____(
+    pipeline = Pipeline(
         steps=[
-            ("____", ____),
-            ("____", ____),
-        ],
+            ("preprocessor", columnTransformer),
+            ("classifier", SVC())
+        ]
     )
 
     # Entrene el pipeline con los datos de entrenamiento.
-    ____.____(____, ____)
+    pipeline.fit(X_train, y_train)
 
-    # # Retorne el pipeline entrenado
+    # Retorne el pipeline entrenado
     return pipeline
+
 
 
 def pregunta_04():
@@ -237,7 +233,7 @@ def pregunta_04():
     """
 
     # Importe confusion_matrix
-    from ____ import ____
+    from sklearn.metrics import confusion_matrix
 
     # Obtenga el pipeline de la pregunta 3.
     pipeline = pregunta_03()
@@ -246,15 +242,16 @@ def pregunta_04():
     X_train, X_test, y_train, y_test = pregunta_02()
 
     # Evalúe el pipeline con los datos de entrenamiento usando la matriz de confusion.
-    cfm_train = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_train = confusion_matrix(
+        y_true=y_train,
+        y_pred=pipeline.predict(X_train)
     )
 
-    cfm_test = ____(
-        y_true=____,
-        y_pred=____.____(____),
+    cfm_test = confusion_matrix(
+        y_true=y_test,
+        y_pred=pipeline.predict(X_test)
     )
 
     # Retorne la matriz de confusion de entrenamiento y prueba
     return cfm_train, cfm_test
+
